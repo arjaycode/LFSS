10b729e0bcc3bb8656aaa2911446ade4611721d1
ARJAY ARANAS <aranasarjay1@gmail.com>
2025-09-09 22:33:36 +0800

    head icon

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FindersKeeperz - Login</title>
+  <link rel="icon" type="image/x-icon" href="{{ asset('img/Logo.svg') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
   {{-- <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" /> --}}
   @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/welcome.css'])
